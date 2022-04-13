e6da14d04153b4c40f72b6123b85f792a517e1e8
范云飞 <fyf786452470@gmail.com>
2022-04-13 16:58:09 +0800

    更新类案例

---
+++ ruby primary/rubyClassExample.rb
@@ -0,0 +1,29 @@
+
+class Customer
+	@@no_of_customers = 0
+	def initialize(id, name, addr)
+		@cust_id = id 
+		@cust_name = name
+		@cust_addr = addr
+	end 
+
+	def display_defails()
+		puts "Customer id #{@cust_id}"
+		puts "Customer name #{@cust_name}"
+		puts "Customer address #{@cust_addr}"
+	end
+
+	def totol_no_of_customers()
+		@@no_of_customers += 1
+		puts "Totol number of customers: #{@@no_of_customers}"
+	end 
+end 
+
+cust1 = Customer.new("1", "john", "wisdom apartments, ludhiya")
+cust2 = Customer.new("2","poul","new empire road, khandala")
+
+cust1.display_defails
+cust1.totol_no_of_customers
+
+cust2.display_defails
+cust2.totol_no_of_customers
