9d191dea81ccf17124f5c841b1cbae4b3197d2bb
范云飞 <fyf786452470@gmail.com>
2022-04-14 16:31:26 +0800

    更新string

---
+++ ruby primary/rubyString.rb
@@ -0,0 +1,14 @@
+# 单引号字符串
+
+
+## 如果你要在单引号字符串内使用单引号字符，那么需要在单引号字符串使用反斜杠（\）
+
+puts 'Won\'t  you read 0 \'Reilly\'s book?' 
+
+
+# 双引号字符串
+
+name1 = "Joe"
+name2 = "Mary"
+
+puts "你好 #{name1}, #{name2} 在哪？"
